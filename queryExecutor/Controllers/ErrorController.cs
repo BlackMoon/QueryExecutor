4f7e42f1c3057132c839a8a1aee66c184644d881
Роман Романов <romanovrv@tatintec.ru>
2017-01-12 16:36:35 +0300

    ErrorController

---
+++ queryExecutor/Controllers/ErrorController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Web.Mvc;
+using queryExecutor.Domain;
+
+namespace queryExecutor.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        protected override void HandleUnknownAction(string code)
+        {
+            int statusCode;
+            int.TryParse(code, out statusCode);
+
+            ErrorMessage msg = new ErrorMessage()
+            {
+                Code = statusCode,
+                Description = Enum.GetName(typeof(HttpStatusCode), statusCode)
+            };
+
+            View("Error", msg).ExecuteResult(ControllerContext);
+
+            Response.StatusCode = statusCode;
+        }
+
+        // Handle 401: Unauthorized
+        public ActionResult Unauthorized()
+        {
+            ErrorMessage msg = new ErrorMessage()
+            {
+                Code = 401,
+                Description = Enum.GetName(typeof(HttpStatusCode), 401)
+            };
+
+            return View("Error", msg);
+        }
+    }
+}
---
+++ queryExecutor/Domain/ErrorMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace queryExecutor.Domain
+{
+    public class ErrorMessage
+    {
+        public int Code { get; set; }
+
+        public string Description { get; set; }
+    }
+}
