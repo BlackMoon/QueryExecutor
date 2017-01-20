731ec8e82907f73260a18ccb036b5b4dc385f0e7
Роман Романов <romanovrv@aquilon-st.ru>
2017-01-20 09:12:34 +0300

    BehaiorHostFactory

---
+++ queryExecutor/Service/BehaviorHostFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using DryIoc;
+using Serilog;
+
+namespace queryExecutor.Service
+{
+    /// <summary>
+    /// Фабрика сервисов
+    /// </summary>
+    public class BehaviorHostFactory : ServiceHostFactory
+    {
+        /// <summary>
+        /// Creates a <see cref="BehaviorHost"/> for a specified type of service with a specific base address. 
+        /// </summary>
+        /// <returns>
+        /// A <see cref="BehaviorHost"/> for the type of service specified with a specific base address.
+        /// </returns>
+        /// <param name="serviceType">
+        /// Specifies the type of service to host. 
+        /// </param>
+        /// <param name="baseAddresses">
+        /// The <see cref="T:System.Array"/> of type <see cref="T:System.Uri"/> that contains the base addresses for the service hosted.
+        /// </param>
+        protected override ServiceHost CreateServiceHost(Type serviceType, Uri[] baseAddresses)
+        {
+            //Register the service as a type so it can be found from the instance provider
+            Startup.Container.Register(serviceType);
+
+            return new BehaviorHost(serviceType, baseAddresses);
+        }
+    }
+}
